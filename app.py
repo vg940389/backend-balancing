a2da431ee38f6940f91834eb10c3803b4b9d92d9
Venkat Golakoti <venkat.g93@gmail.com>
2026-02-12 16:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/api/hello')
 def hello_world():
     """Returns Hello, EDP!"""
-    with open("etc/hostname" ,"r") as f:
+    with open("/etc/hostname" ,"r") as f:
         hostname=f.read().strip()
     return f"Response received from pod: {hostname}"
 
